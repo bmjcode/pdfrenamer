d80aa1c95d7c2c369f841b52975a3b3c574ae3e5
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2018-10-14 14:56:01 -0400

    Move dependency checking to pdfrenamer.py

--- pdfrenamer.py
+++ pdfrenamer.py
@@ -2,6 +2,71 @@
 
 """A simple utility to preview and rename documents."""
 
-if __name__ == "__main__":
+import os
+import sys
+
+try:
+    # Python 3
+    from tkinter import Tk
+    from tkinter.messagebox import showerror
+except (ImportError):
+    # Python 2
+    from Tkinter import Tk
+    from tkMessageBox import showerror
+
+try:
     from pdfrenamer import main
-    main()
+except (ImportError):
+    # Indicate we need to handle the error condition
+    main = None
+
+
+if __name__ == "__main__":
+    if main:
+        main()
+
+    else:
+        # Check for missing dependencies not in the standard library
+        missing_deps = []
+
+        # Dependency: tkDocViewer
+        try: import tkdocviewer
+        except (ImportError): missing_deps.append("tkDocViewer")
+
+        # Dependency: userpaths
+        try: import userpaths
+        except (ImportError): missing_deps.append("userpaths")
+
+        # If pip is present, we'll suggest how to install the needed modules
+        try: import pip
+        except (ImportError): pip = None
+
+        if missing_deps:
+            # Format the error message
+            bulleted_deps = ["    * {0}".format(dep)
+                             for dep in missing_deps]
+            message = ("Your system is missing the following Python "
+                       "module(s) required to run this program:\n"
+                       "\n"
+                       "{0}"
+                      ).format("\n".join(bulleted_deps))
+
+            if pip:
+                # Always suggest the command-line version of Python
+                python_exe = sys.executable.replace("pythonw", "python")
+
+                message += ("\n"
+                            "\n"
+                            "To install them, try running:\n"
+                            "{0} -m pip install {1}"
+                           ).format(python_exe, " ".join(missing_deps))
+
+            # Hide the root window when calling showerror()
+            root = Tk()
+            root.withdraw()
+
+            # Display an error message
+            showerror("Missing Dependencies", message, parent=root)
+
+            # Exit with an error
+            sys.exit(1)
--- pdfrenamer/__init__.py
+++ pdfrenamer/__init__.py
@@ -2,11 +2,20 @@
 
 """A simple utility to preview and rename documents."""
 
+import os
+import sys
+
+from glob import iglob
+
 try:
-    from .ui import PDFRenamer
+    # Python 3
+    from tkinter import TclError
 except (ImportError):
-    # This lets main() know to handle the error condition
-    PDFRenamer = None
+    # Python 2
+    from Tkinter import TclError
+
+from .ui import PDFRenamer
+
 
 # Export the PDFRenamer widget and our main function
 __all__ = ["PDFRenamer", "main"]
@@ -15,96 +24,35 @@ __all__ = ["PDFRenamer", "main"]
 def main():
     """Start the PDF Renamer application."""
 
-    import os
-    import sys
-
-    from glob import iglob
+    # Start the application
+    r = PDFRenamer()
 
     try:
-        # Python 3
-        from tkinter import Tk, TclError
-        from tkinter.messagebox import showerror
-    except (ImportError):
-        # Python 2
-        from Tkinter import Tk, TclError
-        from tkMessageBox import showerror
+        # Start with the window maximized
+        r.wm_state("zoomed")
 
-    if PDFRenamer:
-        # Start the application
-        r = PDFRenamer()
+    except (TclError):
+        # ...this isn't available on all platforms
+        pass
 
-        try:
-            # Start with the window maximized
-            r.wm_state("zoomed")
-
-        except (TclError):
-            # ...this isn't available on all platforms
-            pass
-
-        # Parse command line arguments
-        if len(sys.argv) > 1:
-            if os.path.isdir(sys.argv[-1]):
-                # If the last command line argument is a folder, then
-                # open all files found in that folder
-                r.open_dir(os.path.abspath(sys.argv[-1]))
-
-            else:
-                # Open each file specified on the command line
-                paths = []
-                for arg in sys.argv[1:]:
-                    for item in iglob(arg):
-                        paths.append(os.path.abspath(item))
-
-                r.open(*paths)
+    # Parse command line arguments
+    if len(sys.argv) > 1:
+        if os.path.isdir(sys.argv[-1]):
+            # If the last command line argument is a folder, then
+            # open all files found in that folder
+            r.open_dir(os.path.abspath(sys.argv[-1]))
 
         else:
-            # Browse for a folder after the main loop has started
-            r.after(50, r.browse)
+            # Open each file specified on the command line
+            paths = []
+            for arg in sys.argv[1:]:
+                for item in iglob(arg):
+                    paths.append(os.path.abspath(item))
 
-        r.mainloop()
+            r.open(*paths)
 
     else:
-        # Check for missing dependencies not in the standard library
-        missing_deps = []
+        # Browse for a folder after the main loop has started
+        r.after(50, r.browse)
 
-        # Dependency: tkDocViewer
-        try: import tkdocviewer
-        except (ImportError): missing_deps.append("tkDocViewer")
-
-        # Dependency: userpaths
-        try: import userpaths
-        except (ImportError): missing_deps.append("userpaths")
-
-        # If pip is present, we'll suggest how to install the needed modules
-        try: import pip
-        except (ImportError): pip = None
-
-        if missing_deps:
-            # Format the error message
-            bulleted_deps = ["    * {0}".format(dep)
-                             for dep in missing_deps]
-            message = ("Your system is missing the following Python "
-                       "module(s) required to run this program:\n"
-                       "\n"
-                       "{0}"
-                      ).format("\n".join(bulleted_deps))
-
-            if pip:
-                # Always suggest the command-line version of Python
-                python_exe = sys.executable.replace("pythonw", "python")
-
-                message += ("\n"
-                            "\n"
-                            "To install them, try running:\n"
-                            "{0} -m pip install {1}"
-                           ).format(python_exe, " ".join(missing_deps))
-
-            # Hide the root window when calling showerror()
-            root = Tk()
-            root.withdraw()
-
-            # Display an error message
-            showerror("Missing Dependencies", message, parent=root)
-
-            # Exit with an error
-            sys.exit(1)
+    r.mainloop()
