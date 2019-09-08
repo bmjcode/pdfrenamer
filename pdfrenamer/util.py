a9ee55d5ca24843be17095620c1cac835dcf9321
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2019-09-08 16:28:01 -0400

    Use the improved find_executable() from tkDocViewer

--- pdfrenamer/util.py
+++ pdfrenamer/util.py
@@ -4,11 +4,15 @@ import os
 import sys
 
 
-def find_executable(basename, search_dirs=None):
+def find_executable(basenames, search_dirs=None):
     """Find the specified executable.
 
-    The suffix ".exe" will automatically be appended to the basename
-    on Microsoft Windows systems.
+    The basenames argument can be a single name or a list. If
+    multiple basenames are specified, then the search order will
+    be by directory first, then by basename within each directory.
+
+    On Microsoft Windows systems, the ".exe" extension will be
+    appended automatically to basenames with no extension specified.
 
     If no search_dirs are specified, the default is to search all
     directories in the system's $PATH.
@@ -16,21 +20,29 @@ def find_executable(basename, search_dirs=None):
     Returns the full path to the executable if found, None otherwise.
     """
 
-    # Ensure this is, in fact, a basename
-    basename = os.path.basename(basename)
-
-    # Append a file suffix if necessary
-    if sys.platform.startswith("win"):
-        basename += ".exe"
+    if isinstance(basenames, str):
+        # Convert a single basename to a one-element list
+        basenames = [basenames]
 
     if not search_dirs:
         # Search the system $PATH
         search_dirs = os.getenv("PATH").split(os.pathsep)
 
     for search_dir in search_dirs:
-        candidate = os.path.join(search_dir, basename)
-        if os.path.exists(candidate):
-            return candidate
+        for basename in basenames:
+            # Ensure this is, in fact, a basename
+            basename = os.path.basename(basename)
+
+            # Append a file extension if necessary
+            if sys.platform.startswith("win"):
+                base, ext = os.path.splitext(basename)
+                if not ext:
+                    basename = "{0}.exe".format(basename)
+
+            # Check if the candidate executable exists
+            candidate = os.path.join(search_dir, basename)
+            if os.path.isfile(candidate):
+                return candidate
 
 
 try:
@@ -41,12 +53,7 @@ except (AttributeError):
     import subprocess
 
     # Find an appropriate opener for startfile()
-    _startfile_opener = None
-    for basename in ("open", "xdg-open"):
-        cmd = find_executable(basename)
-        if cmd:
-            _startfile_opener = cmd
-            break
+    _startfile_opener = find_executable(["open", "xdg-open"])
 
     def _startfile(path, operation=None):
         """Platform-independent replacement for os.startfile().
